faa40068346c63f44a425f57510ce590ccceba3f
acharayaP03 <acharyaP03@gmail.com>
2023-03-05 10:32:18 +1100

    header componenet with navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,22 @@
 import Link from "next/link"
-import logo from '../public/logo.svg'
+
 function Header() {
   return (
-  <header>
-    <div>
-        <Link href="/">
-           <img src="/logo.svg" alt="" className="w-44 cursor-pointer fill-black p-4"/>
-        </Link>
-    </div>
+  <header className="border-black border-b-[1px]">
+      <nav className="flex items-center p-5 max-w-7xl mx-auto">
+          <div className="flex items-center space-x-5 flex-1">
+              <Link href="/">
+                  <img src="/logo.svg" alt="" className="w-44 cursor-pointer fill-black p-4"/>
+              </Link>
+          </div>
+          <div className="flex items-center space-x-5 font-light text-[14px]">
+              <h3>Our story</h3>
+              <h3>Membership</h3>
+              <h3>Write</h3>
+              <h3>Sign in</h3>
+              <h3 className="text-white bg-black px-4 py-1 rounded-full">Get started</h3>
+          </div>
+      </nav>
   </header>
   )
 }
