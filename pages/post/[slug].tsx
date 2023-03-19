7b928cca767cf7502bc988ec8bbd8d7c3e4c411d
acharayaP03 <acharyaP03@gmail.com>
2023-03-19 21:23:23 +1100

    add comment section to post

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,9 +4,18 @@ import {Posts} from "../../typings";
 import {GetStaticProps} from "next";
 import Navigation from "../../components/Navigation";
 import PortableText from "react-portable-text";
+import Comments from "../../components/Comments";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 interface Props {
     post: Posts
+}
+
+interface FormInput {
+    _id: string,
+    name: string,
+    email: string,
+    comment: string
 }
 
 /**
@@ -47,6 +56,9 @@ export const getStaticProps: GetStaticProps = async ({ params }) =>{
               name,
               image
             },
+            'comment': *[
+                _type == "comment" && post._ref == ^._id && approved == true
+            ],
             description,
             mainImage,
             slug,
@@ -72,8 +84,36 @@ export const getStaticProps: GetStaticProps = async ({ params }) =>{
     }
 }
 
-export default function Post ({ post }: Props) {
+const onSubmit : SubmitHandler<FormInput> = async (data) => {
+    console.log(data)
 
+
+    try{
+         await fetch('/api/createComment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }catch (error){
+        console.log(error)
+    }
+    // fetch('/api/createComment', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(data)
+    // }).then(() => {
+    //     console.log(data)
+    // }).catch((error) => {
+    //     console.log('this one errored out....')
+    // })
+}
+
+export default function Post ({ post }: Props) {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormInput>()
     return (
             <>
                 <div className="border-black border-b-[1px] sticky">
@@ -106,14 +146,62 @@ export default function Post ({ post }: Props) {
                                         <h2 className="text-xl font-bold my-5" {...props} />
                                     ),
                                     li: ({children}: any) =>(
-                                        <li className="ml-4 list-disc">{children}</li>
+                                        <li className="ml-4 list-disc  mt-4">{children}</li>
                                     ),
                                     link: ({ href, children }: any) => (
                                         <a href={href} className="text-blue-500 hover:underline">{ children }</a>
+                                    ),
+                                    img: ({ }) => (
+                                        <img src="" alt=""/>
                                     )
                                 }
                             }
                         />
+                    </div>
+                    <hr className="mt-5 my-r mx-auto border border-yellow-500"/>
+                    <div>
+                        <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10" onSubmit={handleSubmit(onSubmit)}>
+                            <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+                            <h3 className="text-3xl font-bold">Leave a comment below.</h3>
+                            <hr className="py-3 mt-2"/>
+
+                            <input type="hidden" {...register("_id")} name="_id" value={post._id}/>
+                            <div className="block mb-5">
+                                <label>
+                                    <span className="text-gray-700">Name</span>
+                                </label>
+                                <input
+                                    {...register("name", { required: true })}
+                                    type="text"
+                                    placeholder="John doe"
+                                    className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 focus:ring outline-none"/>
+                                { errors.name && (<span className="text-red-500 text-sm">Name field is required!</span>)}
+                            </div>
+                            <div className="block mb-5">
+                                <label>
+                                    <span className="text-gray-700">Email</span>
+                                </label>
+                                <input
+                                    {...register("email", { required: true })}
+                                    type="text"
+                                    placeholder="Type your email here..."
+                                    className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 focus:ring outline-none"/>
+                                { errors.email && (<span className="text-red-500 text-sm">Email field is required!</span>)}
+                            </div>
+                            <div>
+                                <label>
+                                    <span className="text-gray-700">Comments</span>
+                                </label>
+                                <textarea
+                                    {...register("comment", { required: true })}
+                                    placeholder="Comments"
+                                    rows={8}
+                                    className="shadow border rounded py-2 px-3 form-textarea mt-2 block w-full ring-yellow-500 focus:ring outline-none"/>
+                                { errors.comment && (<span className="text-red-500 text-sm">Comment field is required!</span>)}
+                            </div>
+
+                            <input type="submit" className="mt-5 bg-yellow-500 hover:bg-yellow-400 focus:outline-none focus:shadow-outline text-white font-bold py-2 px-4 rounded cursor-pointer"/>
+                        </form>
                     </div>
                 </main>
             </>
